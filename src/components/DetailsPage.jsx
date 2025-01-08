f3fe414239e5b9d8adaff9dcdd396302d20ea6dd
hossein <hosseinbakhtiari781@outlook.com>
2025-01-08 23:31:30 +0330

    develop details page component

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,14 +1,169 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useBookDetails } from "../context/BookContext";
+import { FaPlusCircle, FaShoppingCart, FaUser } from "react-icons/fa";
+import Circles from "../constant/circles";
 
 const DetailsPage = () => {
   const { id } = useParams();
   const bookDetails = useBookDetails(+id);
-  console.log(bookDetails.id);
+  console.log(bookDetails);
   return (
-    <div>
-      {/* <p>{bookDetails.title}</p> */}
+    <div className=" relative flex items-center justify-center bg-[#ebeaea]">
+      <div className="relative overflow-hidden  w-full h-full bg-[#ffffff] p-4">
+        {/* <circles /> */}
+        <div className="sm:hidden absolute top-[-20px] left-[-90px] w-40 h-40 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute top-[-80px] left-[-10px] w-40 h-40 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute top-[240px] left-[-80px] w-80 h-80 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute top-[100px] right-[-90px] w-80 h-80 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute bottom-[-30px] right-[-90px] w-40 h-40 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute bottom-[-90px] right-[-10px] w-40 h-40 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute top-[900px] left-[-80px] w-80 h-80 bg-[#52D8F942] rounded-full opacity-90"></div>
+        <div className="sm:hidden absolute top-[700px] right-[-90px] w-80 h-80 bg-[#52D8F942] rounded-full opacity-90"></div>
+
+        {/* buttons*/}
+        <div className="relative flex justify-between items-center mb-4 mx-2">
+          <div className="flex flex-row ">
+            <button className="flex items-center bg-[#89C9D4] px-6 py-2 font-semibold rounded-3xl">
+              Back
+            </button>
+            <div className="ml-2 flex items-center justify-center rounded-full bg-[#89C9D4] w-10 h-10">
+              <FaShoppingCart />
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="mr-2 flex items-center justify-center rounded-full bg-[#89C9D4] w-10 h-10">
+              <FaUser />
+            </div>
+            <button className="bg-[#89C9D4]  px-6 py-2 rounded-3xl font-semibold">
+              Log Out
+            </button>
+          </div>
+        </div>
+
+        {/* Search Box */}
+        <div class="relative mx-3 my-5">
+          <input
+            type="text"
+            placeholder="Search Books"
+            class="w-full shadow-lg placeholder:font-semibold px-4 py-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <button
+            type="button"
+            class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-4.35-4.35M16.24 16.24a7 7 0 10-9.9-9.9 7 7 0 009.9 9.9z"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className="relative bg-white p-6 rounded-lg shadow-md max-w-3xl mx-auto my-8">
+          <div className="flex flex-col md:flex-row gap-6">
+            {/* Book*/}
+            <div className="flex flex-row justify-center rounded-xl  overflow-hidden ">
+              <div className="flex flex-row w-2/5 mr-2">
+                <div className="flex items-center justify-center  ">
+                  <img
+                    src={bookDetails.image}
+                    alt={bookDetails.title}
+                    className=""
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col justify-between p-4 w-3/4">
+                <h2 className="text-lg font-bold">{bookDetails.title}</h2>
+                <div className="flex font-semibold flex-row my-2">
+                  <p className="text-sm mr-1">by</p>
+                  <p className="text-sm text-[#2BB9D2]">{bookDetails.author}</p>
+                </div>
+                <p className="text-sm font-semibold text-gray-700">
+                  {bookDetails.description}
+                </p>
+              </div>
+            </div>
+
+            {/* Product Description*/}
+            <div className="flex-1">
+              <p className="text-gray-600 mb-4">by {bookDetails.author}</p>
+
+              <h2 className="text-lg font-semibold mt-4 mb-2">
+                Product Description
+              </h2>
+              <p className="text-gray-700 mb-4">
+                {bookDetails.product_description}
+              </p>
+
+              <p className="text-gray-500 text-sm mb-2">
+                <span className="font-semibold">Publication Date:</span>{" "}
+                {bookDetails.publicationDate}
+              </p>
+              <p className="text-gray-500 text-sm mb-4">
+                <span className="font-semibold">Product :</span>{" "}
+                {bookDetails.id}
+              </p>
+              <p className="text-gray-500 text-sm mb-4">
+                <span className="font-semibold">Pages:</span>{" "}
+                {bookDetails.pages}
+              </p>
+            </div>
+          </div>
+
+          {/* Price and buttons*/}
+          <div className="mt-6 border-t pt-4">
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              ${bookDetails.price} (USD)
+            </p>
+            <div className="flex gap-4 items-center mb-4 flex-wrap">
+              <div>
+                <label className="text-sm font-semibold block mb-1">
+                  Formats:
+                </label>
+                <select className="border px-3 py-2 rounded-md">
+                  <option>Ebook</option>
+                  <option>Hardcover/Hardcopy</option>
+                </select>
+              </div>
+              <div>
+                <label className="text-sm font-semibold block mb-1">
+                  Languages:
+                </label>
+                <select className="border px-3 py-2 rounded-md">
+                  <option>English</option>
+                  <option>Spanish</option>
+                </select>
+              </div>
+              <div>
+                <label className="text-sm font-semibold block mb-1">
+                  Quantity:
+                </label>
+                <input
+                  type="number"
+                  min="1"
+                  defaultValue="1"
+                  className="border px-3 py-2 rounded-md w-16"
+                />
+              </div>
+            </div>
+            <button className="w-full bg-red-500 text-white py-3 rounded-md font-bold shadow-md hover:bg-red-600">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/constant/book.jsx
+++ src/constant/book.jsx
@@ -8,8 +8,12 @@ const book = [
         "Fiction",
         "Classic"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "July 11, 2017",
+    "pages": 288,
+    "price": 35.0,
     "description": "A classic novel depicting racial injustice in the American South.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    "image": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1500191671i/61663.jpg"
 },
 {
     "id": 2,
@@ -20,8 +24,12 @@ const book = [
         "Dystopian",
         "Science Fiction"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "February 11, 2017",
+    "pages": 168,
+    "price": 25.0,
     "description": "A dystopian novel portraying a totalitarian society.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    "image": "https://www.bookshare.org/wp-content/uploads/2024/01/1P6VYKiu24lytaX_rQ-_fBDfu1Oqx5C5NSTE8CdyGhY-MEDIUM.jpg"
 },
 {
     "id": 3,
@@ -32,8 +40,12 @@ const book = [
         "Classic",
         "Romance"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "July 11, 2017",
+    "pages": 288,
+    "price": 35.0,
     "description": "A classic novel exploring themes of love, marriage, and social norms.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/01/ft8-sOmjeroYDBN_DT6NDjT-NPYiDyJo7v67FCEdNl8-MEDIUM.jpg"
 },
 {
     "id": 4,
@@ -44,8 +56,12 @@ const book = [
         "Fiction",
         "Classic"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "January 20, 2023",
+    "pages": 188,
+    "price": 40.6,
     "description": "A tale of the American Dream, wealth, and love during the Roaring Twenties.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/02/not-dead-enough-12.jpg"
 },
 {
     "id": 5,
@@ -56,8 +72,12 @@ const book = [
         "Fiction",
         "Adventure"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "July 11, 2017",
+    "pages": 288,
+    "price": 35.0,
     "description": "The epic tale of Captain Ahab's obsession with the white whale.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/01/ug6tQEFLNeak2lrWoGAwtdxOWWGzd3glpSV5WvkDPYE-MEDIUM.jpg"
 },
 {
     "id": 6,
@@ -68,8 +88,12 @@ const book = [
         "Fantasy",
         "Adventure"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "January 20, 2023",
+    "pages": 188,
+    "price": 40.6,
     "description": "An epic fantasy saga about the quest to destroy the One Ring.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/01/p-C-Jf1y7QwTOAuu2hYEfWn7lqMpiFytwcG72f436ys-MEDIUM.jpg"
 },
 {
     "id": 7,
@@ -80,8 +104,12 @@ const book = [
         "Fiction",
         "Coming-of-age"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "March 3, 2012",
+    "pages": 588,
+    "price": 47.0,
     "description": "A classic coming-of-age novel following Holden Caulfield's journey.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/01/UB1DBlxcqMdXVeCGMdSw6mBrD2gOwk-KdCx6dDqu8NU-MEDIUM.jpg"
 },
 {
     "id": 8,
@@ -92,8 +120,12 @@ const book = [
         "Fantasy",
         "Adventure"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "February 11, 2017",
+    "pages": 168,
+    "price": 25.0,
     "description": "The prequel to The Lord of the Rings, following Bilbo Baggins' journey.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/05/liliana-9780593244111.jpeg"
 },
 {
     "id": 9,
@@ -104,8 +136,12 @@ const book = [
         "Magical Realism",
         "Literary Fiction"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "January 20, 2023",
+    "pages": 188,
+    "price": 40.6,
     "description": "A classic novel depicting racial injustice in the American South.",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/01/MY5HdIkd8suZP7_MqzZvmmNyl36YUcttekgGH9qxs7o-MEDIUM.jpg"
 },
 {
     "id": 10,
@@ -116,8 +152,12 @@ const book = [
         "Historical Fiction",
         "Epic"
     ],
+    "product_description": "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh varius ornare dis neque penatibus. Urna aliquam tristique quis nec nostra dictumst. Mauris ullamcorper auctor dui consectetur primis urna. Scelerisque venenatis lobortis nascetur tristique viverra nullam. Curae ligula dictum pharetra parturient rhoncus. Tincidunt condimentum nascetur turpis pellentesque, pellentesque vel facilisi.Sapien auctor mauris euismod montes malesuada placerat pulvinar amet. Posuere placerat potenti donec laoreet tristique dignissim. Massa non metus eget suscipit porttitor etiam. Metus nunc consectetur justo leo ante ut aptent quisque habitasse. Adipiscing donec tempus, malesuada montes convallis nunc. Porttitor quam egestas dui conubia fermentum semper. Ipsum per elementum efficitur nunc velit cubilia. Nam fermentum praesent erat id maecenas dui rhoncus arcu.Nostra morbi feugiat et nam pulvinar vulputate class. Fermentum placerat adipiscing morbi egestas ultrices sapien eleifend facilisi. Inceptos quisque dis pellentesque, proin senectus venenatis. Quisque hendrerit felis risus euismod risus hendrerit fringilla!",
+    "publicationDate": "January 12, 2013",
+    "pages": 215,
+    "price": 25.0,
     "description": "A classic coming-of-age novel following Holden Caulfield's journey..",
-    image: "https://fakeimg.pl/667x1000/cc6600"
+    image: "https://www.bookshare.org/wp-content/uploads/2024/01/4OqugRbur4kCYMyadaLiIrZGaYYrJApR92HK6LuzlbU-MEDIUM.jpg"
 },
 ];
 
